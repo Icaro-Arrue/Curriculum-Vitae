7f985b1959b2d1b27c19f566ae07c8bb4da4fc69
Icaro-Arrue <liviapereira0516@gmail.com>
2023-10-20 21:13:19 -0300

    Add files via upload

---
+++ cv/script.js
@@ -0,0 +1,71 @@
+function darkMode () {
+    var elemento = document.body;
+    elemento.classList.toggle("dark-mode")
+}
+
+
+function aparecer () {
+    var elemento = document.getElementById("desc")
+    elemento.classList.toggle("aparecer")
+}
+
+function aparecer1(){
+    var el1 = document.getElementById("desc1")
+    el1.classList.toggle("aparecer")
+}
+
+function aparecer2(){
+    var el2 = document.getElementById("desc2")
+    el2.classList.toggle("aparecer")
+}
+
+function aparecer3(){
+    var el3 = document.getElementById("desc3")
+    el3.classList.toggle("aparecer")
+}
+
+function aparecer4(){
+    var el4 = document.getElementById("desc4")
+    el4.classList.toggle("aparecer")
+    
+}
+
+
+
+/*var elemento = document.getElementsByClassName("desc")
+
+elemento.forEach(element => {
+element.addEventListener( 
+    "click", () => {
+        element.classList.toggle("aparecer")
+    })
+})
+*/
+
+
+/* errei fui mlk 
+let topico = document.getElementsByClassName("bt")
+let descricao = document.getElementsByClassName("desc")
+
+topico.forEach((element) => {
+    element.addEventListener(
+        "click", () => {
+            let descricao = document.getElementsByClassName("desc")
+           descricao.classList.toggle("aparecer")
+        }    )
+});
+
+
+var aa = document.getElementById("botaoTexto")
+var bb = document.getElementById("botao")
+
+
+aa.addEventListener(
+    "click", () => {
+        aa.classList.toggle("colorir")
+    }
+)
+
+forEach((element) => lembrar
+*/
+
